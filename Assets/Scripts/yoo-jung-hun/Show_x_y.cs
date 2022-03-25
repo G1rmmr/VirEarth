ae9aaf232f167026c7d000a025000c5e6911f692
JungHun1 <92992916+JungHun1@users.noreply.github.com>
2022-03-25 16:23:27 +0900

    print pointer finger xy

---
+++ Assets/Scripts/yoo-jung-hun/Show_x_y.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Show_x_y : MonoBehaviour
+{
+    public Text showXY;
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowFingerXY(index);
+    }
+
+    private void ShowFingerXY(int index)
+    {
+        float x = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints[index].x;
+        float y = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints[index].y;
+        showXY.text = "Index : " + index + " | x = (" + x.ToString("F4") + "), y = (" + y.ToString("F4") + ")";
+    }
+}
