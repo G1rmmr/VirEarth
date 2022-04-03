eebcecfb92a26149961243c4dc4ea2cd7f50de39
JungHun1 <92992916+JungHun1@users.noreply.github.com>
2022-04-03 17:50:14 +0900

    0403

--- Assets/Scripts/yoo-jung-hun/HandTracking.cs
+++ Assets/Scripts/yoo-jung-hun/HandTracking.cs
@@ -15,14 +15,16 @@ public class HandTracking : MonoBehaviour
     public static float zvalue;       // landmark 0번의 z value
 
 
-    // private
+    // private 변수
     private GestureInfo gesture;        // 제스처
+    private bool inventoryOnFlag;
 
     // Start is called before the first frame update
     void Start()
     {
         HandImage.enabled = false;
         isHandOn = false;
+        inventoryOnFlag = false;
     }
 
     // Update is called once per frame
@@ -36,7 +38,9 @@ public class HandTracking : MonoBehaviour
         HandImage.enabled = true;
         isHandOn = true;
 
-        gesture = ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info;
+        test.text = "It is Debug Text\n";
+        InventoryOn();
+        /*gesture = ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info;
         test.text = "Hand_side : " + gesture.hand_side.ToString() + " || is_right = " + gesture.is_right.ToString() + "\n";
         if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.is_right == 1) // 오른손이면
             test.text += "Right Hand\n";
@@ -52,9 +56,75 @@ public class HandTracking : MonoBehaviour
         else if (((int)gesture.mano_class) == 2)
             test.text += "Pointer\n";
         else
-            test.text += "Gestrue?";
+            test.text += "Gestrue?";*/
     }
 
+    private void InventoryOn()
+    {
+        if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.hand_side != HandSide.Palmside)
+            return;
+        if (IsFoldFinger(false, false, false, false, false))
+        {
+            test.text += "Inventory ON\n";
+        }
+        else
+            test.text += "Inventory OFF\n";
+        return;
+    }
 
+    private bool IsFoldFinger(bool thumb, bool point, bool big, bool four, bool little) // 엄지, 검지, 중지, 약지, 새끼
+    {
+        int count = 0;
+        TrackingInfo hand = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info;
+        //hand.skeleton.joints[0].x
+        if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.is_right == 1) // 오른손이면
+        {
+            if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.hand_side == HandSide.Backside) // 손등이면
+            {
+                if ((hand.skeleton.joints[4].x > hand.skeleton.joints[2].x) == (thumb)) { }
+                else
+                    return false;
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.hand_side == HandSide.Palmside)    // 손바닥이면
+            {
+                if ((hand.skeleton.joints[4].x < hand.skeleton.joints[2].x) == (thumb)) { }
+                else
+                    return false;
+            }
+        }
+        else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.is_right == 0) // 왼손이면
+        {
+            if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.hand_side == HandSide.Backside) // 손등이면
+            {
+                if ((hand.skeleton.joints[4].x < hand.skeleton.joints[2].x) == (thumb)) { }
+                else
+                    return false;
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.hand_side == HandSide.Palmside)    // 손바닥이면
+            {
+                if ((hand.skeleton.joints[4].x > hand.skeleton.joints[2].x) == (thumb)) { }
+                else
+                    return false;
+            }
+        }
+
+        if ((hand.skeleton.joints[8].y < hand.skeleton.joints[6].y) == (point)) { } // 검지
+        else
+            return false;
+
+        if ((hand.skeleton.joints[12].y < hand.skeleton.joints[10].y) == (point)) { } // 중지
+        else
+            return false;
+
+        if ((hand.skeleton.joints[16].y < hand.skeleton.joints[14].y) == (point)) { } // 약지
+        else
+            return false;
+
+        if ((hand.skeleton.joints[20].y < hand.skeleton.joints[18].y) == (point)) { } // 새끼
+        else
+            return false;
+
+        return true;
+    }
 
 }
